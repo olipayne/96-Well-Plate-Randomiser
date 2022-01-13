8196e1a47b4e67de25ae0a3d51016ce52d42d0aa
Oliver Payne <opayne@ripe.net>
2022-01-13 22:42:29 +0100

    remove unneeded import

--- 96-well-plate-randomiser.py
+++ 96-well-plate-randomiser.py
@@ -22,7 +22,6 @@ control_wells = [("1", "A1"), ("2", "A1"), ("3", "A1")]
 
 import csv
 import random
-from shutil import copyfile
 
 letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
